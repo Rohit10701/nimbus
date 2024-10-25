235af20cd5e83dfb5fc9c188db09b95682eb8294
Rohit <rohitkumar10701@gmail.com>
2024-10-25 07:33:49 +0530

    chore : added tanstack/router with vite

--- packages/nimbus/src/main.tsx
+++ packages/nimbus/src/main.tsx
@@ -2,10 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { routeTree } from './routeTree.gen'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { Provider } from 'react-redux'
+import { store } from './libs/redux/store.ts'
 
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
 
--- packages/nimbus/vite.config.ts
+++ packages/nimbus/vite.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from 'vite'
 import path from 'node:path'
 import electron from 'vite-plugin-electron/simple'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    // react(),
+    TanStackRouterVite(),
+    viteReact(),
     electron({
       main: {
         // Shortcut of `build.lib.entry`.
