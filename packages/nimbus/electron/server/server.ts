4a3d046311175895ba552e7f274b6559a7c0d183
Rohit <rohitkumar10701@gmail.com>
2024-10-30 23:07:03 +0530

    chore : removed dist-electron from the git

--- packages/nimbus/electron/server/server.ts
+++ packages/nimbus/electron/server/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { mockApiRouter } from './routes/mockSchemaRoutes'
 import * as dotenv from "dotenv"
+import { db } from './config/db'
 dotenv.config()
 
 const PORT = 3001
@@ -12,6 +13,25 @@ function nodeServer() {
 	app.use(cors())
 
 	app.use('/mockSchemaApi', mockApiRouter)
+	app.post('/mockSchema/:id', async (req, res) => {
+		const { id } = req.params;
+		const {page, limit} 
+		try {
+			const row = await db('MockApiData').where({ id }).first();
+	
+			if (row) {
+				res.status(200).json({
+					id : row.id,
+					data : JSON.parse(row.data)
+				});
+			} else {
+				res.status(404).json({ error: 'Data not found' });
+			}
+		} catch (error) {
+			console.error('Database error:', error);
+			res.status(500).json({ error: 'Database error' });
+		}
+	});
 	app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 }
 
